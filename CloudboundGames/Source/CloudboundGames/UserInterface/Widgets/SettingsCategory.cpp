5288913601f6366ac82a3ab7764b0a6d1af1a03b
emluperry <emluperry@gmail.com>
2025-04-21 18:02:10 +0100

    223 - Default and Save Settings
    - Created a Settings Category class in C++ that the Audio/Controls/Video menus inherit from. This provides two new blueprint native events to reset to defaults and save the current settings.
    - Moved Settings menus into their own section of the settings menu.
    - Settings menu bg now hidden when seen in-game, replaced by a blur
    - Life icon in the HUD now scales.
    - Buttons on the settings menu hooked up to the blueprint native events, though unfortunately uses a cast to access the other widgets. Could look into converting this logic into C++ for performance reasons, into a blueprint interface, or otherwise. Either way, the functionality has not been implemented otherwise, with the exception of the video settings which already had a function for saving the selected settings.

---
+++ CloudboundGames/Source/CloudboundGames/UserInterface/Widgets/SettingsCategory.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SettingsCategory.h"
+
+void USettingsCategory::ResetToDefaults_Implementation()
+{
+}
+
+void USettingsCategory::SaveSettings_Implementation()
+{
+}
---
+++ CloudboundGames/Source/CloudboundGames/UserInterface/Widgets/SettingsCategory.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SettingsCategory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLOUDBOUNDGAMES_API USettingsCategory : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void ResetToDefaults();
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void SaveSettings();
+};
