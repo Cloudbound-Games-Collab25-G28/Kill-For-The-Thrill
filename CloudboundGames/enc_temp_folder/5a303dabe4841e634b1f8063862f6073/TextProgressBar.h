00b45926f8cad3effcd55721841527703397eff5
TristanFarr <147660980+TristanLeigh@users.noreply.github.com>
2025-05-01 23:13:27 +0100

    Added Enums for the shop items when chosen

---
+++ CloudboundGames/enc_temp_folder/5a303dabe4841e634b1f8063862f6073/TextProgressBar.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TextProgressBar.generated.h"
+
+class UProgressBar;
+class UTextBlock;
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class CLOUDBOUNDGAMES_API UTextProgressBar : public UUserWidget
+{
+	GENERATED_BODY()
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UProgressBar> Bar;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> ValueText;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void UpdateValue(float newValue, float maxValue);
+};
